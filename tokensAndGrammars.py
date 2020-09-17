f5b1dcc6634794aa58a973be605f91ae74c85905
Marianavv <mariana2v@hotmail.com>
2020-09-16 22:12:09 -0500

    Definition of tokens

---
+++ tokensAndGrammars.py
@@ -0,0 +1,135 @@
+import ply.lex as lex
+import ply.yacc as yacc
+import sys
+
+#Tokens definition
+tokens = [
+    #Arithmetic Operators
+			'MINUS',
+			'PLUS',
+			'MULTIPLY',
+			'DIVIDE',
+            'MOD',
+            'INCREMENT',
+            'DECREMENT',
+    #Relational Operators
+            'COMPARISON',
+            'DIFFERENT',
+            'GREATERTHAN',
+			'LESSTHAN',
+            'GREATERHANOREQUAL',
+            'LESSTHANOREQUAL',
+    #Logical Operators
+            'AND',
+            'OR',   
+    #Assignment Operators         
+			'EQUALS',
+            'PLUSEQUALS',
+            'SUBSTRACTEQUALS',
+    #Others
+            'ID',
+			'COLON',
+			'COMMA',
+			'LEFTBRACKET',
+			'RIGHTBRACKET',
+			'LEFTPARENTHESIS',
+			'RIGHTPARENTHESIS',
+            'SEMICOLON',
+            'CTESTRING',
+            'CTECHAR',
+            'CTEFLOAT',
+            'CTEINT'
+    ]
+#Reserved words definition
+reserved = {
+    'int': 'INT',
+    'float': 'FLOAT',
+    'var': 'VAR',
+    'module': 'MODULE',
+    'return': 'RETURN',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'do': 'DO',
+    'if': 'IF',
+    'else': 'ELSE',
+    'read': 'READ',
+    'write': 'WRITE',
+    'print': 'PRINT', #Lleva?
+    'program': 'PROGRAM'
+}
+
+tokens += list(reserved.values())
+
+#Token expressions
+t_MINUS = r'\-'
+t_PLUS = r'\+'
+t_MULTIPLY = r'\*'
+t_DIVIDE = r'\/'
+t_MOD = r'\%'
+t_INCREMENT = r'\+\+'
+t_DECREMENT = r'\-\-'
+
+t_COMPARISON = r'\=\='
+t_GREATERHANOREQUAL = r'\>\='
+t_LESSTHANOREQUAL = r'\<\='
+t_GREATERTHAN = r'\>'
+t_LESSTHAN = r'\<'
+t_DIFFERENT = r'\!\=' 
+
+t_AND = r'\&' 
+t_OR = r'\|' 
+
+t_EQUALS = r'\='
+t_PLUSEQUALS = r'\+\=' 
+t_SUBSTRACTEQUALS = r'\-\=' 
+t_LEFTBRACKET = r'\{'
+t_RIGHTBRACKET = r'\}'
+t_LEFTPARENTHESIS = r'\('
+t_RIGHTPARENTHESIS= r'\)'			
+t_COMMA = r'\,'
+t_COLON = r'\:'
+t_SEMICOLON = r'\;'
+t_ignore = ' \t\n'
+
+def t_CTESTRING(token): #To be fixed--------------------------------
+    #r'\"(.*)\"'
+    r'"([^"\n]|(\\"))*"$'
+    token.value = str(token.value)
+    return token
+
+def t_CTECHAR(token): #To be fixed----------------------------------
+    r'\"([a-zA-Z])\"'
+    token.value = chr(token.value)
+    return token
+
+def t_CTEFLOAT(t):
+    r'\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value, 'ID')
+    return t
+
+def t_CTEINT(t):
+   r'\d+'
+   t.value = int(t.value)
+   return t
+
+lexer = lex.lex()
+#lexer.input("- + * / % ++ -- == != > < >= <= & | = += -= resultado : ; , { } ( ) 32 32.55 A abc")
+while True:
+    tok = lexer.token()
+    if not tok:
+        break
+    print(tok)
+
+#Grammars Definitions
+
+def p_program(p):
+    '''
+    program : PROGRAM ID SEMICOLON vars bloque
+            | PROGRAM ID SEMICOLON bloque
+    '''
+    pass
