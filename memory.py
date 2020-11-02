ccd48f137d71c4adfdb214facfd74417e5b7193e
Marianavv <mariana2v@hotmail.com>
2020-11-02 02:21:47 -0600

    Counters to keep track of memory needed in execution

--- memory.py
+++ memory.py
@@ -1,6 +1,15 @@
 class memory:
     def __init__(self):
+        """
+
+        """
         self.globalMemory = {
+            'int' = {},
+            'float' = {},
+            'bool' = {},
+            'char' = {}
+        }
+        self.globalTemporalMemory = {
             'int' = {},
             'float' = {},
             'bool' = {},
@@ -12,7 +21,7 @@ class memory:
             'bool' = {},
             'char' = {}
         }
-        self.temporalMemory = {
+        self.localTemporalMemory = {
             'int' = {},
             'float' = {},
             'bool' = {},
@@ -32,17 +41,47 @@ class memory:
 
         Each memory context will be divided into types context (int, bool, float, char, temp)
         avoiding the use of casting values.
-
+        
         """
-        self.globalMem = 1000
-        self.localMem = 17000
-        self.constantMem = 33000
-        self.temporalMem = 49000
+        self.globalBase = 1000
+        self.globalTempBase = 17000 
+        self.localBase = 33000
+        self.localTempBase = 49000
+        self.constantBase = 65000
+
         self.intMem = 0
         self.floatMem = 4000
         self.charMem = 8000
         self.boolMem = 12000
         self.topLimit = 15999
+
+        """
+        Counters will help to keep record of how much memory will be needed in execution
+        """
+        globalIntCounter = 0
+        globalFloatCounter = 0
+        globalBoolCounter = 0
+        globalCharCounter = 0
+
+        tempGlobalIntCounter = 0
+        tempGlobalFloatCounter = 0
+        tempGlobalBoolCounter = 0
+        tempGlobalCharCounter = 0
+
+        localIntCounter = 0
+        localFloatCounter = 0
+        localBoolCounter = 0
+        localCharCounter = 0
+
+        tempLocalIntCounter = 0
+        tempLocalFloatCounter = 0
+        tempLocalBoolCounter = 0
+        tempLocalCharCounter = 0
+        
+        constantIntCounter = 0
+        constantFloatCounter = 0
+        constantBoolCounter = 0
+        constantCharCounter = 0
 
     def saveVariableGlobal(self,variables):
         """
@@ -65,18 +104,58 @@ class memory:
         """
          for var in variables:
             if type(var) is bool:
-                self.globalMemory[self.globalMem+self.boolMem] = var
+                self.globalMemory[self.globalBase+self.boolMem] = var
                 self.boolMem += 1
+                self.globalBoolCounter +=1
             elif type(var) is int:
-                self.globalMemory[self.globalMem+self.intMem] = var
+                self.globalMemory[self.globalBase+self.intMem] = var
                 self.intMem += 1
+                self.globalIntCounter +=1
             elif type(var) is float:
-                self.globalMemory[self.globalMem+self.floatMem] = var
+                self.globalMemory[self.globalBase+self.floatMem] = var
                 self.floatMem += 1
+                self.globalFloatCounter +=1
             else:
-                self.globalMemory[self.globalMem+self.charMem] = var
+                self.globalMemory[self.globalBase+self.charMem] = var
                 self.charMem += 1
+                self.globalCharCounter +=1
     
+    def saveVariableGlobalTemp(self,variables):
+        """
+        Saves values into the Temporal Global context depending on the type of value
+        being save (int, float, char, bool)
+        
+        Example: globalTemporalMemory = {
+                                'int': {
+                                        '8001' :'32',
+                                        '8002' :'24'
+                                        },
+                                'float' :{
+                                        '5002' :'55',
+                                        '5003' :'63'
+                                        }
+                                 }
+
+        Args:
+            variables (string): list with the values to store.
+        """
+         for var in variables:
+            if type(var) is bool:
+                self.globalTemporalMemory[self.globalTempBase+self.boolMem] = var
+                self.boolMem += 1
+                self.tempGlobalBoolCounter +=1
+            elif type(var) is int:
+                self.globalTemporalMemory[self.globalTempBase+self.intMem] = var
+                self.intMem += 1
+                self.tempGlobalIntCounter +=1
+            elif type(var) is float:
+                self.globalTemporalMemory[self.globalTempBase+self.floatMem] = var
+                self.floatMem += 1
+                self.tempGlobalFloatCounter +=1
+            else:
+                self.globalTemporalMemory[self.globalTempBase+self.charMem] = var
+                self.charMem += 1
+                self.tempGlobalCharCounter +=1
 
     def saveVariableLocal(self,variables):
          """
@@ -99,24 +178,28 @@ class memory:
         """
          for var in variables:
             if type(var) is bool:
-                self.localMemory[self.localMem+self.boolMem] = var
+                self.localMemory[self.localBase+self.boolMem] = var
                 self.boolMem += 1
+                self.localBoolCounter +=1
             elif type(var) is int:
-                self.localMemory[self.localMem+self.intMem] = var
+                self.localMemory[self.localBase+self.intMem] = var
                 self.intMem += 1
+                self.localIntCounter +=1
             elif type(var) is float:
-                self.localMemory[self.localMem+self.floatMem] = var
+                self.localMemory[self.localBase+self.floatMem] = var
                 self.floatMem += 1
+                self.localFloatCounter +=1
             else:
-                self.localMemory[self.localMem+self.charMem] = var
+                self.localMemory[self.localBase+self.charMem] = var
                 self.charMem += 1
+                self.localCharCounter +=1
 
-    def saveVariableTemporal(self,variables):
+    def saveVariableLocalTemp(self,variables):
          """
-        Saves values into the Temporal context depending on the type of value
+        Saves values into the Local Temporal context depending on the type of value
         being save (int, float, char, bool)
         
-        Example: temporalMemory = {
+        Example: localTemporalMemory = {
                                 'int': {
                                         '8001' :'32',
                                         '8002' :'24'
@@ -132,20 +215,24 @@ class memory:
         """
          for var in variables:
             if type(var) is bool:
-                self.temporalMemory[self.temporalMem+self.boolMem] = var
+                self.localTemporalMemory[self.localTempBase+self.boolMem] = var
                 self.boolMem += 1
+                self.tempLocalBoolCounter +=1
             elif type(var) is int:
-                self.temporalMemory[self.temporalMem+self.intMem] = var
+                self.localTemporalMemory[self.localTempBase+self.intMem] = var
                 self.intMem += 1
+                self.tempLocalIntCounter +=1
             elif type(var) is float:
-                self.temporalMemory[self.temporalMem+self.floatMem] = var
+                self.localTemporalMemory[self.localTempBase+self.floatMem] = var
                 self.floatMem += 1
+                self.tempLocalFloatCounter +=1
             else:
-                self.temporalMemory[self.temporalMem+self.charMem] = var
+                self.localTemporalMemory[self.localTempBase+self.charMem] = var
                 self.charMem += 1
+                self.tempLocalCharCounter +=1            
 
     def saveVariableConstant(self,variables):
-         """
+        """
         Saves values into the Constant context depending on the type of value
         being save (int, float, char, bool)
         
@@ -165,18 +252,37 @@ class memory:
         """
          for var in variables:
             if type(var) is bool:
-                self.constantsMemory[self.constantMem+self.boolMem] = var
+                self.constantsMemory[self.constantBase+self.boolMem] = var
                 self.boolMem += 1
+                self.constantBoolCounter +=1
             elif type(var) is int:
-                self.constantsMemory[self.constantMem+self.intMem] = var
+                self.constantsMemory[self.constantBase+self.intMem] = var
                 self.intMem += 1
+                self.constantIntCounter +=1
             elif type(var) is float:
-                self.constantsMemory[self.constantMem+self.floatMem] = var
+                self.constantsMemory[self.constantBase+self.floatMem] = var
                 self.floatMem += 1
+                self.constantFloatCounter +=1
             else:
-                self.constantsMemory[self.constantMem+self.charMem] = var
+                self.constantsMemory[self.constantBase+self.charMem] = var
                 self.charMem += 1
+                self.constantCharCounter +=1
     
+        """
+        Reinitialize values for local context and temporal local context so other function can start de count.
+
+        """
+
+    def resetLocalCounters(self):
+        self.localIntCounter = 0
+        self.localFloatCounter = 0
+        self.localBoolCounter = 0
+        self.localCharCounter = 0
+
+        self.tempLocalIntCounter = 0
+        self.tempLocalFloatCounter = 0
+        self.tempLocalBoolCounter = 0
+        self.tempLocalCharCounter = 0
 
     def get_value(self, address,context, context_type ):
          """
@@ -200,41 +306,50 @@ class memory:
         Args:
             mem_address: Memory address 
         """
-            if( mem_address >= self.globalMem and mem_address < self.localMem):
-                if(mem_address >= (self.globalMem + self.intMem) and mem_address < (self.globalMem + self.floatMem)):
+            if( mem_address >= self.globalBase and mem_address < self.globalTempBase):
+                if(mem_address >= (self.globalBase + self.intMem) and mem_address < (self.globalBase + self.floatMem)):
                     return self.get_value(mem_address, self.globalMemory, 'int')
-                if(mem_address >= (self.globalMem + self.floatMem) and mem_address <(self.globalMem + self.charMem)):
+                if(mem_address >= (self.globalBase + self.floatMem) and mem_address <(self.globalBase + self.charMem)):
                     return self.get_value(mem_address, self.globalMemory, 'float')
-                if(mem_address >= (self.globalMem + self.charMem) and mem_address < (self.globalMem + self.boolMem)):
+                if(mem_address >= (self.globalBase + self.charMem) and mem_address < (self.globalBase + self.boolMem)):
                     return self.get_value(mem_address, self.globalMemory, 'char')
-                if(mem_address >= (self.globalMem + self.boolMem) and mem_address < (self.globalMem + self.tempMem)):
-                    return self.get_value(mem_address, self.globalMemory, 'bool')    
-            if( mem_address >= self.localMem and mem_address < self.temporalMem):
-                if(mem_address >= (self.localMem + self.intMem) and mem_address < (self.localMem + self.floatMem)):
+                if(mem_address >= (self.globalBase + self.boolMem) and mem_address < (self.globalBase + self.topLimit)):
+                    return self.get_value(mem_address, self.globalMemory, 'bool')
+            if( mem_address >= self.globalTempBase and mem_address < self.localBase):
+                if(mem_address >= (self.globalTempBase + self.intMem) and mem_address < (self.globalTempBase + self.floatMem)):
+                    return self.get_value(mem_address, self.globalTemporalMemory, 'int')
+                if(mem_address >= (self.globalTempBase + self.floatMem) and mem_address <(self.globalTempBase + self.charMem)):
+                    return self.get_value(mem_address, self.globalTemporalMemory, 'float')
+                if(mem_address >= (self.globalTempBase + self.charMem) and mem_address < (self.globalTempBase + self.boolMem)):
+                    return self.get_value(mem_address, self.globalTemporalMemory, 'char')
+                if(mem_address >= (self.globalTempBase + self.boolMem) and mem_address < (self.globalTempBase + self.topLimit)):
+                    return self.get_value(mem_address, self.globalTemporalMemory, 'bool')    
+            if( mem_address >= self.localBase and mem_address < self.localTempBase):
+                if(mem_address >= (self.localBase + self.intMem) and mem_address < (self.localBase + self.floatMem)):
                     return self.get_value(mem_address, self.localMemory, 'int')
-                if(mem_address >= (self.localMem + self.floatMem) and mem_address < (self.localMem + self.charMem)):
+                if(mem_address >= (self.localBase + self.floatMem) and mem_address < (self.localBase + self.charMem)):
                     return self.get_value(mem_address, self.localMemory, 'float')
-                if(mem_address >= (self.localMem + self.charMem) and mem_address < (self.localMem + self.boolMem)):
+                if(mem_address >= (self.localBase + self.charMem) and mem_address < (self.localBase + self.boolMem)):
                     return self.get_value(mem_address, self.localMemory, 'char')
-                if(mem_address >= (self.localMem + self.boolMem) and mem_address < (self.localMem + self.tempMem)):
-                    return self.get_value(mem_address, self.localMemory, 'bool')    
-            if( mem_address >= self.temporalMem and mem_address < self.constantMem):
-                if(mem_address >= (self.temporalMem + self.intMem) and mem_address < (self.temporalMem + self.floatMem)):
-                    return self.get_value(mem_address, self.temporalMemory, 'int')
-                if(mem_address >= (self.temporalMem + self.floatMem) and mem_address < (self.temporalMem + self.charMem)):
-                    return self.get_value(mem_address, self.temporalMemory, 'float')
-                if(mem_address >= (self.temporalMem + self.charMem) and mem_address < (self.temporalMem + self.boolMem)):
-                    return self.get_value(mem_address, self.temporalMemory, 'char')
-                if(mem_address >= (self.temporalMem + self.boolMem) and mem_address < (self.temporalMem + self.tempMem)):
-                    return self.get_value(mem_address, self.temporalMemory, 'bool')    
-            if( mem_address >= self.constantMem and mem_address < (self.constantMem + self.topLimit)):
-                if(mem_address >= (self.constantMem + self.intMem) and mem_address < (self.constantMem + self.floatMem)):
-                    return self.get_value(mem_address, self.constantMem, 'int')
-                if(mem_address >= (self.constantMem + self.floatMem) and mem_address < (self.constantMem + self.charMem)):
-                    return self.get_value(mem_address, self.constantMem, 'float')
-                if(mem_address >= (self.constantMem + self.charMem) and mem_address < (self.constantMem + self.boolMem)):
-                    return self.get_value(mem_address, self.constantMem, 'char')
-                if(mem_address >= (self.constantMem + self.boolMem) and mem_address < (self.constantMem + self.tempMem)):
-                    return self.get_value(mem_address, self.constantMem, 'bool')    
+                if(mem_address >= (self.localBase + self.boolMem) and mem_address < (self.localBase + self.topLimit)):
+                    return self.get_value(mem_address, self.localMemory, 'bool')        
+            if( mem_address >= self.localTempBase and mem_address < self.constantBase):
+                if(mem_address >= (self.localTempBase + self.intMem) and mem_address < (self.localTempBase + self.floatMem)):
+                    return self.get_value(mem_address, self.localTemporalMemory, 'int')
+                if(mem_address >= (self.localTempBase + self.floatMem) and mem_address < (self.localTempBase + self.charMem)):
+                    return self.get_value(mem_address, self.localTemporalMemory, 'float')
+                if(mem_address >= (self.localTempBase + self.charMem) and mem_address < (self.localTempBase + self.boolMem)):
+                    return self.get_value(mem_address, self.localTemporalMemory, 'char')
+                if(mem_address >= (self.localTempBase + self.boolMem) and mem_address < (self.localTempBase + self.topLimit)):
+                    return self.get_value(mem_address, self.localTemporalMemory, 'bool') 
+            if( mem_address >= self.constantBase and mem_address < (self.constantBase + self.topLimit)):
+                if(mem_address >= (self.constantBase + self.intMem) and mem_address < (self.constantBase + self.floatMem)):
+                    return self.get_value(mem_address, self.constantMemory, 'int')
+                if(mem_address >= (self.constantBase + self.floatMem) and mem_address < (self.constantBase + self.charMem)):
+                    return self.get_value(mem_address, self.constantMemory, 'float')
+                if(mem_address >= (self.constantBase + self.charMem) and mem_address < (self.constantBase + self.boolMem)):
+                    return self.get_value(mem_address, self.constantMemory, 'char')
+                if(mem_address >= (self.constantBase + self.boolMem) and mem_address < (self.constantBase + self.topLimit)):
+                    return self.get_value(mem_address, self.constantMemory, 'bool')    
         
        
